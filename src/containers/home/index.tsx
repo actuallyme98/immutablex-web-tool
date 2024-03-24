ea9ce6e9d037754cb1b0dc994fc91a162f578ef0
Brian <vhoangd860201@gmail.com>
2024-03-24 13:15:11 +0700

    v2.9.1

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -47,7 +47,7 @@ const HomePage: React.FC = () => {
     <Box className={styles.root}>
       <div>
         <Typography variant="h2" className={styles.heading}>
-          ImmutableX Web Tools V2.9
+          ImmutableX Web Tools V2.9.1
         </Typography>
 
         <div>
